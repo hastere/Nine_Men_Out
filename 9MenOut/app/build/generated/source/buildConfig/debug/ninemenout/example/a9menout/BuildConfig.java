1d5d24cc8792fb1a7eaff552b60a514ef25ca300
jjgospodarek <47115194+jjgospodarek@users.noreply.github.com>
2019-02-05 11:30:57 -0600

    Add files via upload
    Blank Android App

---
+++ 9MenOut/app/build/generated/source/buildConfig/debug/ninemenout/example/a9menout/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package ninemenout.example.a9menout;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "ninemenout.example.a9menout";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
