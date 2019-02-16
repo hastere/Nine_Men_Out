9a31f1ddce7874a108b25f09f205a603ae40f0eb
Joe <jjgospodarek!@crimson.ua.edu>
2019-02-16 16:31:09 -0600

    Create RegistrationActivity.java

---
+++ app/src/main/java/com/example/ninemenout/RegistrationActivity.java
@@ -0,0 +1,12 @@
+package com.example.ninemenout;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class RegistrationActivity extends AppCompatActivity {
+
+    private Button button; 
+}
