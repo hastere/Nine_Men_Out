1a8b1f178c34e7c6b02925ec065b0576f3de84af
bpmartinez <bpmartinez@crimson.ua.edu>
2019-02-18 17:26:50 -0600

    Update MainActivity.java
    main activity fundamentals finished, waiting for register new user class

--- app/src/main/java/com/example/ninemenout/MainActivity.java
+++ app/src/main/java/com/example/ninemenout/MainActivity.java
@@ -4,10 +4,11 @@ import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
-import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
+import android.util.Log;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
@@ -15,62 +16,101 @@ import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-    private FirebaseAuth mAuth;
+        private TextView emailLogin;
+        private TextView passwordLogin;
+        private FirebaseAuth mAuth;
+        private static final String TAG = "MainActivity";
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
+        @Override
+        protected void onCreate (Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         // ...
         // Initialize Firebase Auth
         mAuth = FirebaseAuth.getInstance();
 
-        final TextView emailLogin = (TextView) findViewById(R.id.emailLogin);
-        final TextView passwordLogin = (TextView) findViewById(R.id.passwordLogin);
-        final Button btnLogin = (Button) findViewById(R.id.btnLogin);
-        final Button btnCreate = (Button) findViewById(R.id.btnCreate);
+        emailLogin = findViewById(R.id.emailLogin);
+        passwordLogin = findViewById(R.id.passwordLogin);
 
-        btnLogin.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                String email = emailLogin.getText().toString();
-                String password = passwordLogin.getText().toString();
-
-                mAuth.signInWithEmailAndPassword(email, password)
-                        .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-                            @Override
-                            public void onComplete(@NonNull Task<AuthResult> task) {
-                                if (task.isSuccessful()) {
-                                    // Sign in success, update UI with the signed-in user's information
-                             //       Log.d(TAG, "createUserWithEmail:success");
-                                    FirebaseUser user = mAuth.getCurrentUser();
-                                    updateUI(user);
-                                    openHomepageActivity();
-                                } else {
-                                    // If sign in fails, display a message to the user.
-                                    Toast.makeText(view.getContext(), "Authentication failed. Incorrect Email or Password", Toast.LENGTH_SHORT).show();
-                                }
-
-                                emailLogin.setText("");
-                                passwordLogin.setText("");
-                            }
-                        });
-            }
-        });
-
-        btnLogin.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                    Intent nextScreen = new Intent(view.getContext(), RegistrationActivity.class);
-                    startActivityForResult(nextScreen, 0);
-            }
-        });
+        findViewById(R.id.btnLogin).setOnClickListener(this);
+        findViewById(R.id.btnCreate).setOnClickListener(this);
     }
 
-    public void openHomepageActivity() {
-        Intent nextScreen = new Intent(this, HomePageActivity.class);
-        startActivityForResult(nextScreen, 0);
+        @Override
+        public void onStart () {
+            super.onStart();
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        updateUI(currentUser);
+    }
+
+        private void login (String email, String password){
+            if (!checkForm()) {
+                return;
+            }
+
+            mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if (task.isSuccessful()) {
+                        // Sign in success, update UI with the signed-in user's information
+                        Log.d(TAG, "signInWithEmail:success");
+                        FirebaseUser user = mAuth.getCurrentUser();
+                        updateUI(user);
+                    } else {
+                        // If sign in fails, display a message to the user.
+                        Log.w(TAG, "signInWithEmail:failure", task.getException());
+                        Toast.makeText(MainActivity.this, "Authentication failed.",
+                                Toast.LENGTH_SHORT).show();
+                        updateUI(null);
+                    }
+
+                }
+            });
+        }
+
+
+        private boolean checkForm () {
+        boolean check = true;
+
+        String email = emailLogin.getText().toString();
+        if (TextUtils.isEmpty(email)) {
+            emailLogin.setError("Required.");
+            check = false;
+        } else {
+            emailLogin.setError(null);
+        }
+
+        String password = passwordLogin.getText().toString();
+        if (TextUtils.isEmpty(password)) {
+            passwordLogin.setError("Required.");
+            check = false;
+        } else {
+            passwordLogin.setError(null);
+        }
+
+        return check;
+    }
+
+        private void updateUI (FirebaseUser user){
+        if (user != null) {
+            Intent nextScreen = new Intent(this, HomePageActivity.class);
+            this.startActivityForResult(nextScreen, 0);
+        } else {
+            Intent nextScreen = new Intent(this, MainActivity.class);
+            this.startActivityForResult(nextScreen, 0);
+        }
+    }
+
+        @Override
+        public void onClick (View v){
+        int i = v.getId();
+        if (i == R.id.btnCreate) {
+            Intent nextScreen = new Intent(this, HomePageActivity.class);
+            this.startActivityForResult(nextScreen, 0);
+        } else if (i == R.id.btnLogin) {
+            login(emailLogin.getText().toString(), passwordLogin.getText().toString());
+        }
     }
 }
