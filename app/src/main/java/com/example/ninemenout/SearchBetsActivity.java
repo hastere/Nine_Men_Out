af9e4a1dbc772977909581ca4f526d52fdf57ed7
oatazic <oatazic@crimson.ua.edu>
2019-02-27 14:49:09 -0600

    Can get data from firestore

--- app/src/main/java/com/example/ninemenout/SearchBetsActivity.java
+++ app/src/main/java/com/example/ninemenout/SearchBetsActivity.java
@@ -1,13 +1,45 @@
 package com.example.ninemenout;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.widget.EditText;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
 public class SearchBetsActivity extends AppCompatActivity {
+
+    private EditText search_bar;
+    private RecyclerView recyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search_bets);
+
+        search_bar = (EditText) findViewById(R.id.search_bar);
+        recyclerView = (RecyclerView) findViewById(R.id.recyclerSearch);
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        db.collection("users")
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                Log.d("Hello",document.getId() + " => " + document.getData());
+
+                            }
+                        } else {
+                            Log.w("Error", "Error getting documents.", task.getException());
+                        }
+                    }
+                });
     }
 }
