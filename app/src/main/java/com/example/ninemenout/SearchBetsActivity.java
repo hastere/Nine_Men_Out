a65c0c9fea2ce60025443f7125f9299970c80e7e
oatazic <oatazic@crimson.ua.edu>
2019-03-03 11:56:12 -0600

    Undoing a bunch of changes, adding gradle dependencies

--- app/src/main/java/com/example/ninemenout/SearchBetsActivity.java
+++ app/src/main/java/com/example/ninemenout/SearchBetsActivity.java
@@ -5,7 +5,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
+import android.view.View;
 import android.widget.EditText;
+import android.widget.ImageButton;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
@@ -17,29 +19,12 @@ public class SearchBetsActivity extends AppCompatActivity {
 
     private EditText search_bar;
     private RecyclerView recyclerView;
+    private ImageButton searchButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search_bets);
-
-        search_bar = (EditText) findViewById(R.id.search_bar);
-        recyclerView = (RecyclerView) findViewById(R.id.recyclerSearch);
-        FirebaseFirestore db = FirebaseFirestore.getInstance();
-        db.collection("users")
-                .get()
-                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
-                    @Override
-                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
-                        if (task.isSuccessful()) {
-                            for (QueryDocumentSnapshot document : task.getResult()) {
-                                Log.d("Hello",document.getId() + " => " + document.getData());
-
-                            }
-                        } else {
-                            Log.w("Error", "Error getting documents.", task.getException());
-                        }
-                    }
-                });
     }
+
 }
