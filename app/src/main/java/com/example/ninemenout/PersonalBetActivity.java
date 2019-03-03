c903db5a57e48aaec372bcc7338d53beeda17fe0
bpmartinez <bpmartinez@crimson.ua.edu>
2019-03-03 12:12:56 -0600

    added myAdapter class
    added myAdapter class for recylce viewer and updated some layouts

--- app/src/main/java/com/example/ninemenout/PersonalBetActivity.java
+++ app/src/main/java/com/example/ninemenout/PersonalBetActivity.java
@@ -2,12 +2,33 @@ package com.example.ninemenout;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
 
 public class PersonalBetActivity extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager layoutManager;
+    private String myDataset;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_personal_bet);
+        recyclerView = findViewById(R.id.personalBetsList);
+
+        // use this setting to improve performance if you know that changes
+        // in content do not change the layout size of the RecyclerView
+        recyclerView.setHasFixedSize(true);
+
+        // use a linear layout manager
+        layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+
+        // specify an adapter (see also next example)
+        mAdapter = new MyAdapter(myDataset);
+        recyclerView.setAdapter(mAdapter);
     }
 }
--- app/src/main/java/com/example/ninemenout/UserProfileActivity.java
+++ app/src/main/java/com/example/ninemenout/UserProfileActivity.java
@@ -1,13 +1,31 @@
 package com.example.ninemenout;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class UserProfileActivity extends AppCompatActivity {
+
+    private Button betsBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_profile);
+
+        betsBtn =(Button) findViewById(R.id.personalBetsBtn);
+        betsBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openPersonalBetActivity();
+            }
+        });
+    }
+
+    public void openPersonalBetActivity() {
+        Intent intent = new Intent(this, PersonalBetActivity.class);
+        startActivity(intent);
     }
 }
