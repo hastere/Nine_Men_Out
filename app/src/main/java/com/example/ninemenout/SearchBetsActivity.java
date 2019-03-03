032b11a9e43c0db6d87730cf5662f6241ecf25ef
oatazic <oatazic@crimson.ua.edu>
2019-03-03 13:08:11 -0600

    data populates by default (no search functionality)

--- app/src/main/java/com/example/ninemenout/SearchBetsActivity.java
+++ app/src/main/java/com/example/ninemenout/SearchBetsActivity.java
@@ -3,24 +3,63 @@ package com.example.ninemenout;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageButton;
 
+import com.firebase.ui.firestore.FirestoreRecyclerOptions;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.Query;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
 
 public class SearchBetsActivity extends AppCompatActivity {
 
+    private FirebaseFirestore db = FirebaseFirestore.getInstance();
+    private CollectionReference betRef = db.collection("openBets");
+
+    private BetsAdapter adapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search_bets);
+
+        setUpRecyclerView();
+
+
     }
 
+    private void setUpRecyclerView() {
+        Query query = betRef.orderBy("odds", Query.Direction.DESCENDING);
+
+        FirestoreRecyclerOptions<Bets> options = new FirestoreRecyclerOptions.Builder<Bets>()
+                .setQuery(query, Bets.class)
+                .build();
+
+        adapter = new BetsAdapter(options);
+
+        RecyclerView recyclerView = findViewById(R.id.recyclerSearch);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setAdapter(adapter);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        adapter.startListening();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        adapter.stopListening();
+    }
 }
