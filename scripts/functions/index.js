7e8976ca6640c205d8238c7bbfd4d490bf293ac6
hastere <hastere@crimson.ua.edu>
2019-03-05 21:12:36 -0600

    js now imports info into db

--- scripts/functions/index.js
+++ scripts/functions/index.js
@@ -1,92 +1,20 @@
-const admin = require('firebase-admin');
-const functions = require('firebase-functions');
-
-admin.initializeApp();
-
-
-// Take the text parameter passed to this HTTP endpoint and insert it into the
-// Realtime Database under the path /messages/:pushId/original
-exports.addMessage = functions.https.onRequest((req, res) => {
-  // Grab the text parameter.
-  const original = req.query.text;
-  // Push the new message into the Realtime Database using the Firebase Admin SDK.
-  return admin.firestore().ref('/messages').push({original: original}).then((snapshot) => {
-    // Redirect with 303 SEE OTHER to the URL of the pushed object in the Firebase console.
-    return res.redirect(303, snapshot.ref.toString());
-  });
+const admin = require('./node_modules/firebase-admin');
+const serviceAccount = require('../../../../Desktop/service-key.json');
+const data = require("../response.json");
+const collectionKey = "games"; //name of the collection
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://your-database.firebaseio.com"
 });
-
-
-
-
-const data = require("../data.json");
-
-
-function isCollection(data, path, depth) {
-  if (
-    typeof data != 'object' ||
-    data == null ||
-    data.length === 0 ||
-    isEmpty(data)
-  ) {
-    return false;
-  }
-
-  for (const key in data) {
-    if (typeof data[key] != 'object' || data[key] == null) {
-      // If there is at least one non-object item then it data then it cannot be collection.
-      return false;
-    }
-  }
-
-  return true;
-}
-
-function isEmpty(obj) {
-  for(const key in obj) {
-    if(obj.hasOwnProperty(key)) {
-      return false;
-    }
-  }
-  return true;
-}
-
-async function upload(data, path) {
-  return await admin.firestore()
-    .doc(path.join('/'))
-    .set(data)
-    .then(() => console.log(`Document ${path.join('/')} uploaded.`))
-    .catch(() => console.error(`Could not write document ${path.join('/')}.`));
-}
-
-
-async function resolve(data, path = []) {
-  if (path.length > 0 && path.length % 2 == 0) {
-    // Document's length of path is always even, however, one of keys can actually be a collection.
-
-    // Copy an object.
-    const documentData = Object.assign({}, data);
-
-    for (const key in data) {
-      // Resolve each collection and remove it from document data.
-      if (isCollection(data[key], [...path, key])) {
-        // Remove a collection from the document data.
-        delete documentData[key];
-        // Resolve a colleciton.
-        resolve(data[key], [...path, key]);
-      }
-    }
-
-    // If document is empty then it means it only consisted of collections.
-    if (!isEmpty(documentData)) {
-      // Upload a document free of collections.
-      await upload(documentData, path);
-    }
-  } else {
-    // Collection's length of is always odd.
-    for (const key in data) {
-      // Resolve each collection.
-      await resolve(data[key], [...path, key]);
-    }
-  }
+const firestore = admin.firestore();
+const settings = {timestampsInSnapshots: true};
+firestore.settings(settings);
+if (data && (typeof data === "object")) {
+Object.keys(data).forEach(docKey => {
+ firestore.collection(collectionKey).doc(docKey).set(data[docKey]).then((res) => {
+    console.log("Document " + docKey + " successfully written!");
+}).catch((error) => {
+   console.error("Error writing document: ", error);
+});
+});
 }
