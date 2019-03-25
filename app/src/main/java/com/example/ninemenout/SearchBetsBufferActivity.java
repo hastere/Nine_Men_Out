57d5706d91de3f50c73fea63192801f838aa7c3c
oatazic <oatazic@crimson.ua.edu>
2019-03-25 13:10:36 -0500

    comment the search bets buffer

--- app/src/main/java/com/example/ninemenout/SearchBetsBufferActivity.java
+++ app/src/main/java/com/example/ninemenout/SearchBetsBufferActivity.java
@@ -11,6 +11,7 @@ import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.TextView;
 
+// controls user input and uses it to tell the search bets activity what to display
 public class SearchBetsBufferActivity extends AppCompatActivity {
 
     String[] toSend = new String[3];
@@ -24,15 +25,18 @@ public class SearchBetsBufferActivity extends AppCompatActivity {
 
         homeTeam = findViewById(R.id.homeTeam);
 
+        // default values mainly used for testing
         toSend[0] = "Alabama";
         toSend[1] = "spread";
         toSend[2] = "odds";
 
     }
 
+    // controls the first set of radio buttons
     public void chooseBetType(View view){
         boolean checked = ((RadioButton) view).isChecked();
 
+        // update the array that will be sent to search bets based on button clicks
         switch(view.getId()) {
             case R.id.spreadButton:
                 if(checked)
@@ -45,9 +49,11 @@ public class SearchBetsBufferActivity extends AppCompatActivity {
         }
     }
 
+    // controls the second set of radio buttons
     public void chooseSortType(View view){
         boolean checked = ((RadioButton) view).isChecked();
 
+        // update array to be sent based on button presses
         switch(view.getId()) {
             case R.id.oddsButton:
                 if(checked)
@@ -60,11 +66,13 @@ public class SearchBetsBufferActivity extends AppCompatActivity {
         }
     }
 
+    // packages up all of the information and sends it to the new activity
     public void displaySearch(View view){
         String teamName = homeTeam.getText().toString();
-        if(!teamName.equals(""))
+        if(!teamName.equals("")) // eliminate entering nothing, will default to Alabama if nothing entered
             toSend[0] = teamName;
 
+        // send the array of information to the new search activity to be displayed
         Bundle b = new Bundle();
         b.putStringArray("terms", toSend);
         Intent intent = new Intent(this, SearchBetsActivity.class);
