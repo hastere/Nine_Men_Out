b4fc4f0307b99e37ce8660879e2b08c55fe900e7
hastere <hastere@crimson.ua.edu>
2019-03-28 12:58:24 -0500

    added activity to place bet based off of tapping on a game

--- app/src/main/java/com/example/ninemenout/place_bet.java
+++ app/src/main/java/com/example/ninemenout/place_bet.java
@@ -14,23 +14,14 @@ public class place_bet extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_place_bet);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = findViewById(R.id.fab);
 
         Intent intent = getIntent();
         String homeTeam = intent.getStringExtra("homeTeam");
         String awayTeam = intent.getStringExtra("awayTeam");
+        String homeTeamSpread = intent.getStringExtra("homeTeamSpread");
+        String overUnder = intent.getStringExtra("overUnder");
 
-
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
 
 }
