3c7cf2d69bf5077834b483cd706a0963f7d3f6af
hastere <hastere@crimson.ua.edu>
2019-03-30 11:16:41 -0500

    added onclick listener

--- app/src/main/java/com/example/ninemenout/gamesAdapter.java
+++ app/src/main/java/com/example/ninemenout/gamesAdapter.java
@@ -9,8 +9,11 @@ import android.widget.TextView;
 
 import com.firebase.ui.firestore.FirestoreRecyclerAdapter;
 import com.firebase.ui.firestore.FirestoreRecyclerOptions;
+import com.google.firebase.firestore.DocumentSnapshot;
+
 public class gamesAdapter extends FirestoreRecyclerAdapter<games, gamesAdapter.gameHolder> {
 
+    private OnItemClickListener listener;
     public gamesAdapter(@NonNull FirestoreRecyclerOptions<games> options) {
         super(options);
     }
@@ -38,7 +41,26 @@ public class gamesAdapter extends FirestoreRecyclerAdapter<games, gamesAdapter.g
             textViewTitle = itemView.findViewById(R.id.text_view_title);
             textViewDescription = itemView.findViewById(R.id.text_view_description);
             textViewPriority = itemView.findViewById(R.id.text_view_priority);
+
+
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    int position = getAdapterPosition();
+                    if (position != RecyclerView.NO_POSITION && listener != null) {
+                        listener.onItemClick(getSnapshots().getSnapshot(position), position);
+                    }
+                }
+            });
         }
+
     }
 
+    public interface OnItemClickListener {
+        void onItemClick(DocumentSnapshot documentSnapshot, int position);
+    }
+
+    public void setOnItemClickListener(OnItemClickListener listener){
+        this.listener = listener;
+    }
 }
