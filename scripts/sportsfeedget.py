9e04f3f41bee4064332f1dc5225cb7c57d3f08d1
hastere <hastere@crimson.ua.edu>
2019-04-01 13:41:29 -0500

    api fetching updates

--- scripts/sportsfeedget.py
+++ scripts/sportsfeedget.py
@@ -9,7 +9,7 @@ import json
 
 def main():
     url = "https://therundown-therundown-v1.p.rapidapi.com/sports/4/events?include=scores+or+teams+or+all_periods"
-    headers={"X-RapidAPI-Key": ""}
+    headers={"X-RapidAPI-Key": "b58d7149f0msh36091f7253015aep1aa685jsn204004568b2d"}
     responseInfo = requests.get(url,headers=headers)
     print("hello")
     bigDict = {}
@@ -36,6 +36,12 @@ def main():
     file_object = open("response.json", "w")
     file_object.write(simplejson.dumps(bigDict, indent=4, sort_keys=True))
     file_object.close()
+    file_object = open("backup.json", "a")
+    file_object.write(simplejson.dumps(bigDict, indent=4, sort_keys=True))
+    file_object.close()
+
+
+    
     return 0
 
 
