426f039132082c641ffc58502b7120d19430370f
Joe <jjgospodarek!@crimson.ua.edu>
2019-04-01 19:50:55 -0500

    Friends Addition 1.0.5
    Made User Profile page dynamic

--- app/src/main/java/com/example/ninemenout/UserProfileActivity.java
+++ app/src/main/java/com/example/ninemenout/UserProfileActivity.java
@@ -1,19 +1,63 @@
 package com.example.ninemenout;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.content.Intent;
+import android.util.Log;
 import android.view.View;
+import android.widget.TextView;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 public class UserProfileActivity extends AppCompatActivity {
+
+    private FirebaseFirestore db = FirebaseFirestore.getInstance();
+    private CollectionReference userRef = db.collection("users");
+    private FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+    private int userpoints = 0;
+    private String uname;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_profile);
+        TextView userBalance = findViewById(R.id.accountBalance);
+        TextView userName = findViewById(R.id.userName);
+        TextView userEmail = findViewById(R.id.userEmail);
+        String name = user.getEmail();
+        if (name != null) {
+            DocumentReference docRef = userRef.document(name);
+            docRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                @Override
+                public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                    if (task.isSuccessful()) {
+                        DocumentSnapshot document = task.getResult();
+                        if (document.exists()) {
+                            // update home page display to represent user data
+                            userpoints = ((Long) document.get("points")).intValue();
+                            uname = (String) document.get("name");
+                            userBalance.setText(((Integer) userpoints).toString());
+                            userEmail.setText(name);
+                            userName.setText(uname);
+                        } else {
+                            Log.d("document error", "No such document");
+                        }
+                    } else {
+                        Log.d("task error", "get failed with ", task.getException());
+                    }
+                }
+            });
+        }
+
     }
 
 
