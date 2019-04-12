ff057490a3520f5fbca93d4dc5cf4a899ba7262c
Joe <jjgospodarek!@crimson.ua.edu>
2019-04-12 11:14:55 -0500

    Friend Bets 2.0.1
    Added view friends bets activity

---
+++ app/src/main/java/com/example/ninemenout/FriendBetsViewerActivity.java
@@ -0,0 +1,13 @@
+package com.example.ninemenout;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FriendBetsViewerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_friend_bets_viewer);
+    }
+}
