145e79b52ae14af7ab2fb00dfd7c4d6d6ce6b9b3
oatazic <oatazic@crimson.ua.edu>
2019-04-12 12:57:24 -0500

    added itemized controller, global should work for leaderboards

--- app/src/main/java/com/example/ninemenout/LeaderboardActivity.java
+++ app/src/main/java/com/example/ninemenout/LeaderboardActivity.java
@@ -1,13 +1,56 @@
 package com.example.ninemenout;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import com.firebase.ui.firestore.FirestoreRecyclerOptions;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.Query;
 
 public class LeaderboardActivity extends AppCompatActivity {
+
+    private FirebaseFirestore db = FirebaseFirestore.getInstance();
+    private CollectionReference userRef = db.collection("users");
+    private LeaderboardsAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_leaderboard);
+        setContentView(R.layout.activity_search_games);
+        setUpRecyclerView();
+    }
+
+    private void setUpRecyclerView() {
+
+        // list users from biggest point total to smallest
+        Query query = userRef.orderBy("points", Query.Direction.DESCENDING);
+
+        FirestoreRecyclerOptions<Users> options = new FirestoreRecyclerOptions.Builder<Users>()
+                .setQuery(query, Users.class)
+                .build();
+
+        adapter = new LeaderboardsAdapter(options);
+
+        RecyclerView recyclerView = findViewById(R.id.globalLeaderboardRecycler);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setAdapter(adapter);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        adapter.startListening();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        adapter.stopListening();
     }
 }
