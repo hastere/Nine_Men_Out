d7c89fb6fb1207e9aebe9ceaa00105238184737d
oatazic <oatazic@crimson.ua.edu>
2019-04-12 12:47:43 -0500

    adding item specifics for leaderboard viewer

---
+++ app/src/main/java/com/example/ninemenout/LeaderboardsAdapter.java
@@ -0,0 +1,53 @@
+package com.example.ninemenout;
+
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+
+import com.firebase.ui.firestore.FirestoreRecyclerAdapter;
+import com.firebase.ui.firestore.FirestoreRecyclerOptions;
+import com.google.firebase.firestore.DocumentSnapshot;
+
+
+
+public class LeaderboardsAdapter extends FirestoreRecyclerAdapter<Users, LeaderboardsAdapter.LeaderboardsUserHolder> {
+    public String TAG = "LeaderboardsAdapter";
+    public LeaderboardsAdapter(@NonNull FirestoreRecyclerOptions<Users> options) {
+        super(options);
+    }
+
+    @Override
+    protected void onBindViewHolder(@NonNull LeaderboardsUserHolder holder, int position, @NonNull Users model) {
+        holder.textViewRank.setText(position);
+        holder.textViewUser.setText(model.getName());
+        holder.textViewPoints.setText(model.getPoints());
+    }
+
+    @NonNull
+    @Override
+    public LeaderboardsUserHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        View v = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.leaderboard_item, viewGroup, false);
+        return new LeaderboardsUserHolder(v);
+    }
+
+    class LeaderboardsUserHolder extends RecyclerView.ViewHolder {
+        TextView textViewRank, textViewUser, textViewPoints;
+
+        public LeaderboardsUserHolder(@NonNull View itemView) {
+            super(itemView);
+            textViewRank = itemView.findViewById(R.id.text_view_rank);
+            textViewUser = itemView.findViewById(R.id.text_view_user);
+            textViewPoints = itemView.findViewById(R.id.text_view_points);
+
+            // dont think I need an on click listener for this one
+        };
+    }
+
+}
+
