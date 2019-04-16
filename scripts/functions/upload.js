4eca3ea8fc1388f6e5531f7a5bca964cbbdba23b
hastere <hastere@crimson.ua.edu>
2019-04-16 12:04:28 -0500

    specific upload script

---
+++ scripts/functions/upload.js
@@ -0,0 +1,24 @@
+const admin = require('./node_modules/firebase-admin');
+const serviceAccount = require('../../../../Desktop/service-key.json');
+const data = require("../response.json");
+const collectionKey = "games"; //name of the collection
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://your-database.firebaseio.com"
+});
+
+const firestore = admin.firestore();
+const settings = {timestampsInSnapshots: true};
+
+firestore.settings(settings);
+if (data && (typeof data === "object")) {
+Object.keys(data).forEach(docKey => {
+
+ firestore.collection(collectionKey).doc(docKey).set(data[docKey]).then((res) => {
+    console.log("Document " + docKey + " successfully written!");
+}).catch((error) => {
+   console.error("Error writing document: ", error);
+});
+});
+}
