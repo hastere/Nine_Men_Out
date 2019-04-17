878927ea600af4fa9f0e3d7674fab5b459460c19
oatazic <oatazic@crimson.ua.edu>
2019-04-17 10:26:31 -0500

    UI color change to represent which leaderboard youre looking at

--- app/src/main/java/com/example/ninemenout/LeaderboardActivity.java
+++ app/src/main/java/com/example/ninemenout/LeaderboardActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
+import android.widget.Button;
 
 import com.firebase.ui.firestore.FirestoreRecyclerOptions;
 import com.google.firebase.firestore.CollectionReference;
